3ced07230ff87d0fa4d65a3043981866e12cb877
brijeshchhatrala01 <dev.brijesh.chhatrala@gmail.com>
2024-03-02 22:44:00 +0530

    reverse number

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_assignment/pages/flutter_screen2.dart';
+import 'package:flutter_ui_assignment/pages/reverse_number.dart';
 
 void main() {
   runApp(const MyApp());
